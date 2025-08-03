8c077244b560bfd6949904100903aa78bc40dd51
OBINECHE PETER <Hoverexs@gmail.com>
2025-08-03 18:59:11 +0100

    Footer updated

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,8 +1,86 @@
-import "./Footer.css"
-const Footer =()=>{
-    return(
-<div className=""></div>
-    )
-}
+import "./Footer.css";
 
-export default Footer
+const Footer = () => {
+  return (
+    <div className="footer">
+        <div className="footerrappar">
+            <div className="first">
+                <img src="https://thehub.io/_nuxt/164db33c3d5c8e1fcf4d94b65b0da148.svg" />
+                <div className="text">
+                    <span>We connect global talents with</span>
+                    <span>career opportunities in Nordic</span>
+                    <span>startups</span>
+                </div>
+                <div className="second">
+                    <img src="https://thehub.io/_nuxt/a045c9a1e883d22bf0e02702305981cd.svg" />
+                    <img src="https://thehub.io/_nuxt/bfc0407764ed5b5242d56923c464613d.svg" />
+                    <img src="https://thehub.io/_nuxt/8aa3588e761834b5c4e8df1ade562b7b.svg" />
+                    <img src="https://thehub.io/_nuxt/9c216d3b193e15902e460883ab649f4f.svg" />
+                    <img src="https://thehub.io/_nuxt/83544871d0e617c8b68a36c29b7e5e10.svg" />
+                </div>
+                <div className="head">
+                    <h3>Main partners</h3>
+                <div className="body">
+                    <div className="f">
+                        <h4>Mesh</h4>
+                        <h4>Community</h4>
+                    </div>
+                    <div className="Join">
+                        <span className="span1">Danske</span>
+                        <span className="span2">Growth</span>
+                    </div>
+                </div>
+                <div className="final">
+                    <span>© 2025 | Developed by Dev Demola & Dev Peter.</span>
+                </div>
+                </div>
+            </div>
+
+                <div className="footertext">
+                    <div className="Slide1">
+                        <h3>For Candidates</h3>
+                        <div className="sub">
+                        <span>Explore jobs</span>
+                        <span>Explore remote jobs</span>
+                        <span>Explore startups</span>
+                        <span>Explore content</span>
+                        </div>
+                    </div>
+                    <div className="Slide1">
+                        <h3>For startups</h3>
+                        <div className="sub">
+                        <span>Overview</span>
+                        <span>Pricing</span>
+                        <span>Scout</span>
+                        <span>Investor list</span>
+                        <span>Embed Career Page</span>
+                        <span>Startup Deals</span>
+                        <span>Community Program</span>
+                        </div>
+                    </div>
+                    <div className="Slide1">
+                        <h3>Company</h3>
+                        <div className="sub">
+                        <span>About</span>
+                        <span>Blog</span>
+                        <span>Contact</span>
+                        <span>Partners</span>
+                        </div>
+                    </div>
+                    <div className="Slide1">
+                        <h3>EXPLORE</h3>
+                        <div className="sub">
+                        <span>Linkedin</span>
+                        <span>Instagram</span>
+                        <span>Facebook</span>
+                        <span>Twitter</span>
+                    </div>
+                </div>    
+
+            </div>
+        </div>    
+    </div>
+  );
+};
+
+export default Footer;
